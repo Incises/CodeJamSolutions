a39df68c4e29f09cebac51a128b6801add9532c5
jswxdzc <jswxdzc@gmail.com>
2016-07-19 15:11:36 +0800

    2016Q A

---
+++ 2016Q/A/A.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+using namespace std;
+int total;
+int line;
+bool handled[10];
+
+void inc(int foo) {
+	while (foo) {
+		handled[foo % 10] = true;
+		foo /= 10;
+	}
+}
+
+int main()
+{
+   cin >> total;
+   //cout << total << endl;
+   for (int i=0; i<total; ++i)
+   {
+      cin >> line;
+      cout << "Case #" << i+1 << ": ";
+      if (!line)
+      {
+         cout << "INSOMNIA" << endl;
+      } else
+      {
+         for (int ctr=0; ctr<sizeof(handled); ctr++)
+         {
+            handled[ctr]=false;
+         }
+         for(int j=1; ; j++)
+         {
+            inc(j*line);
+            int opt = 0;
+            for (int k=0; k < 10; k++)
+               opt += int(handled[k]);
+            if(opt==10)
+            {
+               //if(!handled[j])
+               cout << j*line << endl;
+               break;
+            }
+
+         }
+
+      }
+   }
+   return 0;
+}
